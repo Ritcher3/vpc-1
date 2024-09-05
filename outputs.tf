1b1e0b9e1e85459a71d26c1e183d2bcd0b92e4dd
Ritcher <ritcherstpreux@gmail.com>
2024-09-05 13:50:17 -0400

    fixed output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "public_subnet" {
   value       =  aws_subnet.public
   description = "private subnet cidr"
 }
+
+output "vpc_id" {
+  value       =  aws_vpc.main.id
+  description = "main vpc"
+}
+
+output "first_public_subnet_id" {
+  value = tolist(values(aws_subnet.public))[0].id
+  description = "ID of the first public subnet"
+}
